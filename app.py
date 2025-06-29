1dfb1cdcbff532e79e6bc6dbda6d20a27851d862
Dinesh0394 <dineshbabu0394@gmail.com>
2025-06-29 13:23:30 +0530

    code completed but deployment not done

--- app.py
+++ app.py
@@ -55,3 +55,7 @@ def index():
 if __name__ == "__main__":
 	# app.run(host="0.0.0.0", port = 8080, debug=True)
 	app.run(host="0.0.0.0", port = 8080)
+     
+
+
+# Code complete without deployment instructions.
